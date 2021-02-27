9c559067ef465829c3bf36332291d6d36a11e7b6
Fernando Sequineli <56481201+FernandoSequineli@users.noreply.github.com>
2021-02-27 10:44:42 +0000

    Update Christmas.java

--- Christmas.java
+++ Christmas.java
@@ -1,19 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package greetingcard;
-
 import java.util.ArrayList;
 
-/**
- *
- * @author fernando
- */
+
 public class Christmas extends CardTemplate {
-     
-    
+        
     @Override
     public void peopleDetails(String sender, String receiver) {
         this.sender = sender;
