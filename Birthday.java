81f266279cb21419cdfb6c9a1b1f2152d93e1d4c
Fernando Sequineli <56481201+FernandoSequineli@users.noreply.github.com>
2021-02-27 10:43:57 +0000

    Update Birthday.java

--- Birthday.java
+++ Birthday.java
@@ -1,16 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package greetingcard;
-
 import java.util.ArrayList;
 
-/**
- *
- * @author fernando
- */
 public class Birthday extends CardTemplate {
      
     @Override
